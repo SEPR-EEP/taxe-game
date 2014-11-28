ea81ade953362093d9857f452ff0c10153c39060
Richard Cosgrove <rc1035@york.ac.uk>
2014-11-28 15:36:38 +0000

    Added Junction model

---
+++ src/com/eep/taxe/models/Junction.java
@@ -0,0 +1,5 @@
+package com.eep.taxe.models;
+
+public class Junction extends Vertex {
+
+}
--- src/com/eep/taxe/models/Vertex.java
+++ src/com/eep/taxe/models/Vertex.java
@@ -1,5 +1,27 @@
 package com.eep.taxe.models;
 
 public abstract class Vertex implements VertexInterface {
+	@Override
+	public int getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
+	@Override
+	public int getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setX(int newX) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setY(int newY) {
+		// TODO Auto-generated method stub
+		
+	}
 }
