a7aca7694b8afb6e88d8aac8d8f0461d7da821eb
Richard Cosgrove <rc1035@york.ac.uk>
2014-12-01 15:48:12 +0000

    Ref. #1 Updated Spendable to have quantity

--- src/com/eep/taxe/models/Spendable.java
+++ src/com/eep/taxe/models/Spendable.java
@@ -1,14 +1,41 @@
 package com.eep.taxe.models;
 
 public abstract class Spendable extends Resource {
-
+	
+	private int quantity;
+	
 	/**
 	 * Instantiates a Spendable Resource
 	 * @param name Name of Resource
 	 * @param image Image URI for the Resource
+	 * @param quantity Quantity of the Spendable Resource
 	 */
-	public Spendable(String name, String image) {
+	public Spendable(String name, String image, int quantity) {
 		super(name, image);
+		this.setQuantity(quantity);
+	}
+
+	/** Get Quantity of the Spendable Resource */
+	public int getQuantity() {
+		return quantity;
+	}
+
+	/** 
+	 * Set Quantity of the Spendable Resource
+	 * @param quantity  Quantity of the Spendable Resource
+	 * */
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	/** Increment Quantity of the Spendable Resource */
+	public void incrementQuantity(){
+		this.quantity++;
+	}
+
+	/** Decrement Quantity of the Spendable Resource */
+	public void decrementQuantity(){
+		this.quantity--;
 	}
 
 }
