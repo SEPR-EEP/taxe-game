36455e2b3593a5f2c005136129d5306248c4acf7
Stefan Kokov <sk1056@york.ac.uk>
2015-01-16 14:17:17 +0000

    Implemented setters for TopPanel and LeftPanel classess

--- src/com/eep/taxe/mvc/game/GameView.java
+++ src/com/eep/taxe/mvc/game/GameView.java
@@ -24,6 +24,21 @@ public class GameView extends javax.swing.JFrame {
 	private final int 		WIDTH  	= 1300;
 	private final int 		HEIGHT 	= 720;
 	private final String	TITLE	= "TAxE Game";
+	private TopPanel topPanel;
+	private LeftPanel leftPanel;
+	private RightPanel rightPanel;
+	private BottomPanel bottomPanel;
+	private JLabel mapLabel;
+	private JPanel gameMenuPanel;
+	
+	
+	public JLabel getMapLabel() {
+		return mapLabel;
+	}
+
+	public void setMapLabel(JLabel mapLabel) {
+		this.mapLabel = mapLabel;
+	}
 
 	JPanel mainMapPanel;
 	public JPanel getMainMapPanel() {
@@ -34,8 +49,6 @@ public class GameView extends javax.swing.JFrame {
 		this.mainMapPanel = mainMapPanel;
 	}
 
-	JPanel gameMenuPanel;
-	
 	public JPanel getGameMenuPanel() {
 		return gameMenuPanel;
 	}
@@ -43,12 +56,56 @@ public class GameView extends javax.swing.JFrame {
 	public void setGameMenuPanel(JPanel gameMenuPanel) {
 		this.gameMenuPanel = gameMenuPanel;
 	}
+	
+	
+	public void setTimer(String time){
+		topPanel.setTimer(time);
+	}
+	
+	public void setMissionInfo(String info){
+		topPanel.setMissionInfo(info);
+	}
+	
+	public void setGold(int gold) {
+		topPanel.setGold(gold);
+	}
+	
+	public void setMetal(int metal){
+		topPanel.setMetal(metal);
+	}
+	
+	public void addMenuButtonActionListener(ActionListener buttonListener){
+		topPanel.addMenuButtonActionListener(buttonListener);
+	}
+	
+	public void addDetailsButtonActionListener(ActionListener buttonListener){
+		topPanel.addDetailsButtonActionListener(buttonListener);
+	}
+	
+	
+	public void setAge (String age){
+		leftPanel.setAge(age);
+	}
+	
+	public void setPlayer1Name(String name){
+		leftPanel.setPlayer1Name(name);
+	}
+	
+	public void setPlayer2Name(String name){
+		leftPanel.setPlayer2Name(name);
+	}
+	
+	public void setPlayer1Score(int score){
+		leftPanel.setPlayer1Score(score);
+	}
+	
+	public void setPlayer2Score(int score){
+		leftPanel.setPlayer2Score(score);
+	}
 
-	TopPanel topPanel;
-	LeftPanel leftPanel;
-	RightPanel rightPanel;
-	BottomPanel bottomPanel;
-	JLabel mapLabel;
+
+
+
 	
 	public GameView() {
 		this.setSize(WIDTH, HEIGHT);
@@ -62,7 +119,7 @@ public class GameView extends javax.swing.JFrame {
 		mainMapPanel.setLayout(new BorderLayout(0, 0));
 			
 			mapLabel = new JLabel();
-//			mapLabel.setIcon(new ImageIcon(getClass().getResource("src/resources/MainMap.jpg")));
+			mapLabel.setIcon(new ImageIcon(getClass().getResource("src/resources/MainMap.jpg")));
 			mapLabel.setText("BACKGROUND FAILED TO LOAD");
 			
 		
@@ -95,18 +152,4 @@ public class GameView extends javax.swing.JFrame {
 		this.setVisible(true);
 
 	}
-	
-	public JLabel getMapLabel() {
-		return mapLabel;
-	}
-
-	public void setMapLabel(JLabel mapLabel) {
-		this.mapLabel = mapLabel;
-	}
-
-	public void setTimer(String time){
-		
-	}
-	
-
 }
