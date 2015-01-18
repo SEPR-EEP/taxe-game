629d73bd8fae6bc7ed3a1342d4d92fc3db1dbb20
Plippitt <peter_lippitt@hotmail.co.uk>
2015-01-18 11:39:56 +0000

    Edited MenuView1.

--- src/com/eep/taxe/mvc/menu/MenuView1.java
+++ src/com/eep/taxe/mvc/menu/MenuView1.java
@@ -40,15 +40,56 @@ public class MenuView1 extends JFrame {
 	private final int 		HEIGHT 	= 600;
 	private final String	TITLE	= "Game Menu";
 
-	private JPanel menu 	= null;
-	private JPanel lobby	= null;
-	
-	
-	
-	
+	private JPanel menu;
+	private JPanel lobby;
+	private JPanel EnterGame;
 	private JPanel contentPane;
+	private JPanel MainMenu;
+	private JPanel Credits;
+	private JPanel HTP;
+	private JPanel Connect;
+	private JPanel createGame;
+	private JPanel waiting;
+	
+	
+	private JLabel lblQuit;
+	private JLabel quitbuttonbg;
+	private JLabel lblEnter;
+	private JLabel enterbuttonbg;
+	private JLabel askusername;
+	private JLabel EnterGamebg;
+	
+	private JLabel label;
+	private JLabel label_1;
+	private JLabel label_2;
+	private JLabel label_3;
+	private JLabel label_4;
+	private JLabel label_5;
+	private JLabel label_6;
+	private JLabel label_7;
+	private JLabel Mainmenubg;
+	private JLabel label_9;
+	private JLabel Creditsbg;
+	private JLabel label_11;
+	private JLabel HTPbg;
+	private JLabel label_18;
+	private JLabel label_19;
+	private JLabel label_20;
+	private JLabel label_21;
+	private JLabel Connectbg;
+	private JLabel lblCancel;
+	private JLabel label_15;
+	private JLabel lblCreate;
+	private JLabel label_16;
+	private JLabel CreatGamebg;
+	private JLabel lblNewLabel;
+	private JLabel Waitingingbg;
+	
 	private JTextField usernamebox;
-
+	
+	private JComboBox FilterGame;
+	private JComboBox Difficulty;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -78,38 +119,39 @@ public class MenuView1 extends JFrame {
 		//SCREEN SIZE
 		this.setSize(WIDTH, HEIGHT);
 		this.setTitle(TITLE);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.lobby	= this.createLobby();
 		contentPane.setLayout(null);
+	//}
 		
 		
 		//enter game
-		JPanel EnterGame = new JPanel();
+		EnterGame = new JPanel();
 		EnterGame.setBounds(0, 0, 784, 562);
 		contentPane.add(EnterGame);
 		EnterGame.setLayout(null);
 		
-		JLabel lblQuit = new JLabel("QUIT", SwingConstants.CENTER);
+		lblQuit = new JLabel("QUIT", SwingConstants.CENTER);
 		lblQuit.setBounds(419, 280, 140, 36);
 		EnterGame.add(lblQuit);
 		lblQuit.setForeground(Color.WHITE);
 		lblQuit.setFont(new Font("Franklin Gothic Demi", Font.PLAIN, 18));
 		
-		JLabel quitbuttonbg = new JLabel("");
+		quitbuttonbg = new JLabel("");
 		quitbuttonbg.setBounds(419, 280, 140, 36);
 		EnterGame.add(quitbuttonbg);
-		quitbuttonbg.setIcon(new ImageIcon(getClass().getResource("src/resources/menuview img/btbg.jpg")));
+		quitbuttonbg.setIcon(new ImageIcon(getClass().getResource("/resources/menuview img/btbg.jpg")));
 	
-		JLabel lblEnter = new JLabel("ENTER", SwingConstants.CENTER);
+		lblEnter = new JLabel("ENTER", SwingConstants.CENTER);
 		lblEnter.setBounds(224, 280, 140, 36);
 		EnterGame.add(lblEnter);
 		lblEnter.setFont(new Font("Franklin Gothic Demi", Font.PLAIN, 18));
 		lblEnter.setForeground(Color.WHITE);
 		
-		JLabel enterbuttonbg = new JLabel("");
+		enterbuttonbg = new JLabel("");
 		enterbuttonbg.setBounds(224, 280, 140, 36);
 		EnterGame.add(enterbuttonbg);
-		enterbuttonbg.setIcon(new ImageIcon("src/resources/menuview img/btbg.jpg"));
+		enterbuttonbg.setIcon(new ImageIcon("/resources/menuview img/btbg.jpg"));
 		
 		
 				//username
@@ -119,130 +161,130 @@ public class MenuView1 extends JFrame {
 				usernamebox.setForeground(new Color(0, 0, 0));
 				usernamebox.setColumns(10);
 				
-				JLabel askusername = new JLabel("Please enter a user name!");
+				askusername = new JLabel("Please enter a user name!");
 				askusername.setBounds(220, 104, 343, 35);
 				EnterGame.add(askusername);
 				askusername.setFont(new Font("Franklin Gothic Demi", Font.PLAIN, 30));
 		
 	
-		JLabel EnterGamebg = new JLabel("");
+		EnterGamebg = new JLabel("");
 		EnterGamebg.setBounds(10, 11, 784, 562);
 		EnterGame.add(EnterGamebg);
-		EnterGamebg.setIcon(new ImageIcon("src/resources/menuview img/B1a-Railroad Tracks(1).jpg"));
+		EnterGamebg.setIcon(new ImageIcon("/resources/menuview img/B1a-Railroad Tracks(1).jpg"));
 		
 		String username = usernamebox.getText();
 		
 		
 	
 		//main menu
-		JPanel MainMenu = new JPanel();
+		MainMenu = new JPanel();
 		MainMenu.setLayout(null);
 		MainMenu.setBorder(new EmptyBorder(5, 5, 5, 5));
 		MainMenu.setBounds(0, 0, 784, 562);
 		contentPane.add(MainMenu);
 		
-		JLabel label = new JLabel("QUIT", SwingConstants.CENTER);
+		label = new JLabel("QUIT", SwingConstants.CENTER);
 		label.setForeground(Color.WHITE);
 		label.setFont(new Font("Franklin Gothic Demi", Font.PLAIN, 18));
 		label.setBounds(298, 404, 181, 36);
 		MainMenu.add(label);
 		
-		JLabel label_1 = new JLabel("");
-		label_1.setIcon(new ImageIcon("src/resources/menuview img/btbg.jpg"));
+		label_1 = new JLabel("");
+		label_1.setIcon(new ImageIcon("/resources/menuview img/btbg.jpg"));
 		label_1.setLocation(new Point(224, 280));
 		label_1.setBounds(298, 404, 181, 36);
 		MainMenu.add(label_1);
 		
-		JLabel label_2 = new JLabel("Start Game", SwingConstants.CENTER);
+		label_2 = new JLabel("Start Game", SwingConstants.CENTER);
 		label_2.setForeground(Color.WHITE);
 		label_2.setFont(new Font("Franklin Gothic Demi", Font.PLAIN, 18));
 		label_2.setBounds(298, 104, 181, 36);
 		MainMenu.add(label_2);
 		
-		JLabel label_3 = new JLabel("");
-		label_3.setIcon(new ImageIcon("src/resources/menuview img/btbg.jpg"));
+		label_3 = new JLabel("");
+		label_3.setIcon(new ImageIcon("/resources/menuview img/btbg.jpg"));
 		label_3.setLocation(new Point(298, 104));
 		label_3.setBounds(298, 104, 181, 36);
 		MainMenu.add(label_3);
 		
-		JLabel label_4 = new JLabel("How to play", SwingConstants.CENTER);
+		label_4 = new JLabel("How to play", SwingConstants.CENTER);
 		label_4.setForeground(Color.WHITE);
 		label_4.setFont(new Font("Franklin Gothic Demi", Font.PLAIN, 18));
 		label_4.setBounds(298, 202, 181, 36);
 		MainMenu.add(label_4);
 		
-		JLabel label_5 = new JLabel("");
-		label_5.setIcon(new ImageIcon("src/resources/menuview img/btbg.jpg"));
+		label_5 = new JLabel("");
+		label_5.setIcon(new ImageIcon("/resources/menuview img/btbg.jpg"));
 		label_5.setLocation(new Point(298, 144));
 		label_5.setBackground(Color.BLACK);
 		label_5.setBounds(298, 202, 181, 36);
 		MainMenu.add(label_5);
 		
-		JLabel label_6 = new JLabel("Credits", SwingConstants.CENTER);
+		label_6 = new JLabel("Credits", SwingConstants.CENTER);
 		label_6.setForeground(Color.WHITE);
 		label_6.setFont(new Font("Franklin Gothic Demi", Font.PLAIN, 18));
 		label_6.setBounds(298, 305, 181, 36);
 		MainMenu.add(label_6);
 		
-		JLabel label_7 = new JLabel("");
-		label_7.setIcon(new ImageIcon("src/resources/menuview img/btbg.jpg"));
+		label_7 = new JLabel("");
+		label_7.setIcon(new ImageIcon("/resources/menuview img/btbg.jpg"));
 		label_7.setLocation(new Point(298, 144));
 		label_7.setBackground(Color.BLACK);
 		label_7.setBounds(298, 305, 181, 36);
 		MainMenu.add(label_7);
 		
-		JLabel Mainmenubg = new JLabel("");
-		Mainmenubg.setIcon(new ImageIcon("src/resources/menuview img/B1a-Railroad Tracks(1).jpg"));
+		Mainmenubg = new JLabel("");
+		Mainmenubg.setIcon(new ImageIcon("/resources/menuview img/B1a-Railroad Tracks(1).jpg"));
 		Mainmenubg.setBounds(0, 0, 784, 562);
 		MainMenu.add(Mainmenubg);
 		
 		
 		//credits
-		JPanel Credits = new JPanel();
+		Credits = new JPanel();
 		Credits.setBounds(0, 0, 784, 562);
 		contentPane.add(Credits);
 		Credits.setLayout(null);
 		Credits.setBorder(new EmptyBorder(5, 5, 5, 5));
 		
-		JLabel label_9 = new JLabel("Credits", SwingConstants.CENTER);
+		label_9 = new JLabel("Credits", SwingConstants.CENTER);
 		label_9.setForeground(Color.BLACK);
 		label_9.setFont(new Font("Franklin Gothic Demi", Font.PLAIN, 30));
 		label_9.setBounds(319, 74, 181, 36);
 		Credits.add(label_9);
 		
-		JLabel Creditsbg = new JLabel("");
-		Creditsbg.setIcon(new ImageIcon("src/resources/menuview img/C_D(g)-Railroad Tracks.jpg"));
+		Creditsbg = new JLabel("");
+		Creditsbg.setIcon(new ImageIcon("/resources/menuview img/C_D(g)-Railroad Tracks.jpg"));
 		Creditsbg.setBounds(0, 0, 784, 562);
 		Credits.add(Creditsbg);
 		
 		
 		// how to play
-		JPanel HTP = new JPanel();
+		HTP = new JPanel();
 		HTP.setLayout(null);
 		HTP.setBorder(new EmptyBorder(5, 5, 5, 5));
 		HTP.setBounds(0, 0, 784, 562);
 		contentPane.add(HTP);
 		
-		JLabel label_11 = new JLabel("How To Play", SwingConstants.CENTER);
+		label_11 = new JLabel("How To Play", SwingConstants.CENTER);
 		label_11.setForeground(Color.BLACK);
 		label_11.setFont(new Font("Franklin Gothic Demi", Font.PLAIN, 30));
 		label_11.setBounds(319, 74, 181, 36);
 		HTP.add(label_11);
 		
-		JLabel HTPbg = new JLabel("");
-		HTPbg.setIcon(new ImageIcon("src/resources/menuview img/C_D(g)-Railroad Tracks.jpg"));
+		HTPbg = new JLabel("");
+		HTPbg.setIcon(new ImageIcon("/resources/menuview img/C_D(g)-Railroad Tracks.jpg"));
 		HTPbg.setBounds(0, 0, 784, 562);
 		HTP.add(HTPbg);
 
 		
 
 		//connect
-		JPanel Connect = new JPanel();
+		Connect = new JPanel();
 		Connect.setLayout(null);
 		Connect.setBounds(0, 0, 784, 562);
 		contentPane.add(Connect);
 		
-		JComboBox FilterGame = new JComboBox();
+		FilterGame = new JComboBox();
 		FilterGame.setToolTipText("");
 		FilterGame.setModel(new DefaultComboBoxModel(new String[] {"  Filter Games", "  \u25CF Sort by difficulty", "  \u25CF Sort by Creator"}));
 		FilterGame.setSelectedIndex(0);
@@ -253,41 +295,41 @@ public class MenuView1 extends JFrame {
 		FilterGame.setBounds(593, 68, 140, 36);
 		Connect.add(FilterGame);
 		
-		JLabel label_18 = new JLabel("Host Game", SwingConstants.CENTER);
+		label_18 = new JLabel("Host Game", SwingConstants.CENTER);
 		label_18.setForeground(Color.WHITE);
 		label_18.setFont(new Font("Franklin Gothic Demi", Font.PLAIN, 18));
 		label_18.setBounds(593, 392, 140, 36);
 		Connect.add(label_18);
 		
-		JLabel label_19 = new JLabel("");
-		label_19.setIcon(new ImageIcon("src/resources/menuview img/btbg.jpg"));
+		label_19 = new JLabel("");
+		label_19.setIcon(new ImageIcon("/resources/menuview img/btbg.jpg"));
 		label_19.setBounds(593, 392, 140, 36);
 		Connect.add(label_19);
 		
-		JLabel label_20 = new JLabel("Back", SwingConstants.CENTER);
+		label_20 = new JLabel("Back", SwingConstants.CENTER);
 		label_20.setForeground(Color.WHITE);
 		label_20.setFont(new Font("Franklin Gothic Demi", Font.PLAIN, 18));
 		label_20.setBounds(593, 462, 140, 36);
 		Connect.add(label_20);
 		
-		JLabel label_21 = new JLabel("");
-		label_21.setIcon(new ImageIcon("src/resources/menuview img/btbg.jpg"));
+		label_21 = new JLabel("");
+		label_21.setIcon(new ImageIcon("/resources/menuview img/btbg.jpg"));
 		label_21.setBounds(593, 462, 140, 36);
 		Connect.add(label_21);
 		
-		JLabel Connectbg = new JLabel("");
-		Connectbg.setIcon(new ImageIcon("src/resources/menuview img/connect - Tunnel.jpg"));
+		Connectbg = new JLabel("");
+		Connectbg.setIcon(new ImageIcon("/resources/menuview img/connect - Tunnel.jpg"));
 		Connectbg.setBounds(0, 0, 784, 562);
 		Connect.add(Connectbg);
 		
 		
 		//create game
-		JPanel CreatGame = new JPanel();
-		CreatGame.setBounds(0, 0, 784, 562);
-		contentPane.add(CreatGame);
-		CreatGame.setLayout(null);
+		createGame = new JPanel();
+		createGame.setBounds(0, 0, 784, 562);
+		contentPane.add(createGame);
+		createGame.setLayout(null);
 		
-		JComboBox Difficulty = new JComboBox();
+		Difficulty = new JComboBox();
 		Difficulty.setToolTipText("");
 		Difficulty.setModel(new DefaultComboBoxModel(new String[] {"  Choose difficulty", "  \u25CF  Easy", "  \u25CF  Medium", "  \u25CF  Hard"}));
 		Difficulty.setSelectedIndex(0);
@@ -296,55 +338,55 @@ public class MenuView1 extends JFrame {
 		Difficulty.setFont(new Font("Franklin Gothic Demi", Font.PLAIN, 16));
 		Difficulty.setBackground(Color.DARK_GRAY);
 		Difficulty.setBounds(302, 218, 181, 35);
-		CreatGame.add(Difficulty);
+		createGame.add(Difficulty);
 		
-		JLabel lblCancel = new JLabel("Cancel ", SwingConstants.CENTER);
+		lblCancel = new JLabel("Cancel ", SwingConstants.CENTER);
 		lblCancel.setForeground(Color.WHITE);
 		lblCancel.setFont(new Font("Franklin Gothic Medium", Font.PLAIN, 18));
 		lblCancel.setBounds(179, 293, 140, 36);
-		CreatGame.add(lblCancel);
+		createGame.add(lblCancel);
 		
-		JLabel label_15 = new JLabel("");
-		label_15.setIcon(new ImageIcon("src/resources/menuview img/btbg.jpg"));
+		label_15 = new JLabel("");
+		label_15.setIcon(new ImageIcon("/resources/menuview img/btbg.jpg"));
 		label_15.setLocation(new Point(593, 392));
 		label_15.setBounds(176, 285, 140, 36);
-		CreatGame.add(label_15);
+		createGame.add(label_15);
 		
-		JLabel lblCreate = new JLabel("Create", SwingConstants.CENTER);
+		lblCreate = new JLabel("Create", SwingConstants.CENTER);
 		lblCreate.setForeground(Color.WHITE);
 		lblCreate.setFont(new Font("Franklin Gothic Medium", Font.PLAIN, 18));
 		lblCreate.setBounds(467, 293, 140, 36);
-		CreatGame.add(lblCreate);
+		createGame.add(lblCreate);
 		
-		JLabel label_16 = new JLabel("");
-		label_16.setIcon(new ImageIcon("src/resources/menuview img/btbg.jpg"));
+		label_16 = new JLabel("");
+		label_16.setIcon(new ImageIcon("/resources/menuview img/btbg.jpg"));
 		label_16.setLocation(new Point(593, 392));
 		label_16.setBounds(480, 285, 140, 36);
-		CreatGame.add(label_16);
+		createGame.add(label_16);
 		
-		JLabel CreatGamebg = new JLabel("");
-		CreatGamebg.setIcon(new ImageIcon("src/resources/menuview img/connect - Tunnel.jpg"));
+		CreatGamebg = new JLabel("");
+		CreatGamebg.setIcon(new ImageIcon("/resources/menuview img/connect - Tunnel.jpg"));
 		CreatGamebg.setBounds(0, 0, 784, 562);
-		CreatGame.add(CreatGamebg);
+		createGame.add(CreatGamebg);
 		
 
 		// waiting
-		JPanel Waiting = new JPanel();
-		Waiting.setBounds(0, 0, 784, 562);
-		contentPane.add(Waiting);
-		Waiting.setLayout(null);
+		waiting = new JPanel();
+		waiting.setBounds(0, 0, 784, 562);
+		contentPane.add(waiting);
+		waiting.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Please wait for another player to join!");
+		lblNewLabel = new JLabel("Please wait for another player to join!");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Franklin Gothic Demi Cond", Font.PLAIN, 32));
 		lblNewLabel.setBounds(119, 33, 502, 38);
-		Waiting.add(lblNewLabel);
+		waiting.add(lblNewLabel);
 		lblNewLabel.setForeground(Color.WHITE);
 		
-		JLabel Waitingingbg = new JLabel("");
-		Waitingingbg.setIcon(new ImageIcon("src/resources/menuview img/connect - Tunnel.jpg"));
+		Waitingingbg = new JLabel("");
+		Waitingingbg.setIcon(new ImageIcon("/resources/menuview img/connect - Tunnel.jpg"));
 		Waitingingbg.setBounds(0, 0, 784, 562);
-		Waiting.add(Waitingingbg);
+		waiting.add(Waitingingbg);
 		
 		
 
