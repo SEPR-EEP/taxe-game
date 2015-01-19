79986f79581f70919f041d1190d649f9584999fb
Stefan Kokov <kokov94@gmail.com>
2015-01-19 01:45:00 +0000

    Resized all the icons for the gameview

--- src/com/eep/taxe/mvc/game/GameView.java
+++ src/com/eep/taxe/mvc/game/GameView.java
@@ -4,7 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionListener;
 
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -30,7 +29,6 @@ public class GameView extends javax.swing.JFrame {
 	private JImagePanel gameMenuPanel;
 	JPanel mainMapPanel;
 	
-
 	
 	
 	public GameView() {
